102c3718f3105ba2153b9da1e044e930f18f7586
vergaraCoder <jhonatanvergara072@gmail.com>
2024-12-18 18:33:24 -0500

    feat

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new FilterError());
   app.enableCors({
-    origin: "*",
+    origin:"http://localhost:5173",
     credentials: true,
   });
   
