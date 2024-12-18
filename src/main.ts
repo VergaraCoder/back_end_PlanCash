b9b3b38c8e868fd9bba1f071a75b38f545d8569d
vergaraCoder <jhonatanvergara072@gmail.com>
2024-12-18 18:36:17 -0500

    eat

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FilterError } from './common/errors/filter/exception.filter';
 import * as cookie from 'cookie-parser';
+import * as bodyParser from 'body-parser';
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new FilterError());
+  app.use(bodyParser.json({ limit: '50mb' }));
   app.enableCors({
     origin:"http://localhost:5173",
     credentials: true,
